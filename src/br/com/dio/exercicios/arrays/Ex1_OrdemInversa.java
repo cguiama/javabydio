2d3de7fe60504761330f29f557f4b22507b0c89b
Guilherme <45200933+cguiama@users.noreply.github.com>
2022-01-24 21:17:48 -0300

    Finalização dos exercicios de estruturas de repetição

--- src/br/com/dio/exercicios/arrays/Ex1_OrdemInversa.java
+++ src/br/com/dio/exercicios/arrays/Ex1_OrdemInversa.java
@@ -1,4 +1,25 @@
 package br.com.dio.exercicios.arrays;
 
+/*
+Faça um Programa que leia um vetor de 5 números inteiros
+e mostre-os na ordem inversa.
+ */
+
 public class Ex1_OrdemInversa {
+    public static void main(String[] args) {
+
+        int[] vetor = {-5, -6, 15, 50, 8, 4};
+        System.out.println("Vetor: ");
+        int count = 0;
+        while (count < (vetor.length)) {
+            System.out.print(vetor[count] + " ");
+            count++;
+        }
+
+        System.out.println("\n Vetor: ");
+        for (int i = (vetor.length - 1); i >= 0; i--) {
+            System.out.println(vetor[i] + " ");
+        }
+    }
 }
+
