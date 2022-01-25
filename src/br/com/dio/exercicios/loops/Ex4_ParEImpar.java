3c8155e08a816556834a3a282cdde998677edee7
Guilherme <45200933+cguiama@users.noreply.github.com>
2022-01-24 21:01:13 -0300

    Finalização dos exercicios de estruturas de repetição

--- src/br/com/dio/exercicios/loops/Ex4_ParEImpar.java
+++ src/br/com/dio/exercicios/loops/Ex4_ParEImpar.java
@@ -15,6 +15,7 @@ public class Ex4_ParEImpar {
         int quantNumeros;
         int contador = 0;
         int numero;
+        int quantidadePares = 0, quantidadeImpares = 0;
 
         System.out.println("Quantidade de números: ");
         quantNumeros = scan.nextInt();
@@ -23,7 +24,17 @@ public class Ex4_ParEImpar {
             System.out.println("Número: ");
             numero = scan.nextInt();
 
+            if (numero % 2 == 0) {
+                quantidadePares++;
+            } else {
+                quantidadeImpares++;
+            }
+
+            contador++;
         } while (contador < quantNumeros);
+
+        System.out.println("A quantidade de números impares é: " + quantidadeImpares);
+        System.out.println("A quantidade de números pares é: " + quantidadePares);
 
     }
 }
--- src/br/com/dio/exercicios/loops/Ex5_Tabuada.java
+++ src/br/com/dio/exercicios/loops/Ex5_Tabuada.java
@@ -1,4 +1,30 @@
 package br.com.dio.exercicios.loops;
 
+/*
+Desenvolva um gerador de tabuada
+capaz de gerar a tabuada de qualquer número inteiro.
+O usuário deve informar de qual numero ele deseja ver a tabuada.
+A saída deve ser conforme o exemplo abaixo:
+
+Tabuada de 5:
+5 X 1 = 5
+5 X 2 = 10
+...
+5 X 10 = 50
+ */
+import java.util.Scanner;
+
 public class Ex5_Tabuada {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Qual número você quer ver a tabuada?");
+        int tabuada = scan.nextInt();
+        System.out.println("Segue a tabuada de " + tabuada + "\n");
+
+        for (int i = 0; i <= 10; i++){
+
+            System.out.println(tabuada + " x " + i + " = " + (tabuada*i));
+        }
+
+    }
 }
